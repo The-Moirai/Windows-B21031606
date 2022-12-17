127f43793dba69cce1ceb8ed960e768b4721b77e
The-Moirai <118966903+The-Moirai@users.noreply.github.com>
2022-12-17 20:49:40 +0800

    静态库基础

---
+++ StrStaticLib/StrStaticLib.cpp
@@ -0,0 +1,31 @@
+﻿// StrStaticLib.cpp : 定义静态库的函数。
+//
+
+#include"StrStaticLib.h"
+#include "pch.h"
+namespace StringFuncs
+{
+	char* strcpy(char* src, int x = 0)
+	{
+		int i, n;
+		n = sizeof(src);
+		char* NewString = (char*)malloc(sizeof(char) * n);
+		for (i = 0; *(src + i) != '\0'; i++)
+		{
+			*(NewString + i) = *(src + i);
+		}
+		*(NewString + i) = '\0';
+		return NewString;
+
+	}
+	char* strcat(char* src1, char* src2)
+	{
+		char* NewString = src1;
+		while (*NewString)
+		{
+			NewString++;
+		}
+		while (*NewString++ = *src2++);
+		return NewString;
+	}
+}
