9215b9775404c55787dd532b0bfd0c51236534e9
The-Moirai <118966903+The-Moirai@users.noreply.github.com>
2022-12-18 14:08:41 +0800

    动态库基础

--- StrDll/StrDll.cpp
+++ StrDll/StrDll.cpp
@@ -1,7 +1,11 @@
 #include"pch.h"
 int MyStrcmp(const char* str1, const char* str2)
 {
-	if (strcmp(str1, str2) > 0)
+	if((str1 != NULL) && (str2 == NULL))
+		return 1;
+	else if ((str1 == NULL) && (str2 == NULL))
+		return 0;
+	if (strcmp(str1, str2) >0)
 		return 1;
 	else if (strcmp(str1, str2) == 0)
 		return 0;
